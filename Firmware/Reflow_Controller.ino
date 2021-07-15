5c9a10a0bcc8f283434ea47bf017c402867c71d8
Garrett Brainard <garrett@Friday.localdomain>
2021-07-14 20:49:16 -0400

    Initial commit

---
+++ Firmware/Reflow_Controller.ino
@@ -0,0 +1,85 @@
+/* REFLOW CONTROLLER 
+AUTHOR: GARRETT BRAINARD
+TARGET DEVICE: TEENSY 3.1
+
+---------------------- REQS ----------------------------------------------
+    Need to include:
+    LCD Library
+    SD Card Library 
+    Thermocouple Board: MAX31855
+    */
+
+
+#include <PID_v1.h>
+#include "Adafruit_MAX31855.h"
+#include <LiquidCrystal.h>
+#include <SPI.h>
+
+
+//PIN DEFINITIONS -----------------------------------------------------------
+#define RELAY_PIN	
+#define MAXDO	3
+#define MAXCS	4
+#define MAXCLK	5
+//--------------------------------------------------------------------------
+
+//INITIALIZE THERMOCOUPLE----------------------------------------------------
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+
+//reflow profile init --------------------------------------------------------
+typedef struct profileValues_s {
+  int16_t soakTemp;
+  int16_t soakDuration;
+  int16_t peakTemp;
+  int16_t peakDuration;
+  double  rampUpRate;
+  double  rampDownRate;
+  uint8_t checksum;
+} Profile_t;
+
+Profile_t activeProfile; // the one and only instance
+int activeProfileId = 0;
+
+//--------------------------------------------------------------------------
+
+
+//State Machine -----------------------------------------------------------
+
+typedef enum {
+  None     = 0,
+  Idle     = 1,
+  Settings = 2,
+  Edit     = 3,
+
+  UIMenuEnd = 9,
+
+  RampToSoak = 10,
+  Soak,
+  RampUp,
+  Peak,
+  RampDown,
+  CoolDown,
+
+  Complete = 20,
+
+  Tune = 30
+} State;
+
+
+State currentState  = Idle;
+State previousState = Idle;
+bool stateChanged = false;
+uint32_t stateChangedTicks = 0;
+
+//------------------------------------------------------------------------
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
